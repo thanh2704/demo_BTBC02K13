92757da76a1d309f0602c62a13d60cc2ddcc255c
unknown <thanh150302@gmail.com>
2021-10-17 21:57:42 +0700

    add thtt

---
+++ thtt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int a = 11;
+	int b = 2;
+	int tong = a+b;
+	cout << "tong = " << tong << endl;
+	int hieu = a-b;
+	cout << "hieu = " << hieu << endl;
+	int tich = a*b;
+	cout << "tich = " << tich << endl;
+	float thuong = a/b;
+	thuong = (float) a/b;
+	cout << "thuong = " << thuong << endl;
+	}
